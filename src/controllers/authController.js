6bb008c600cb9d0523f2af17a259f819550964db
gef12 <gerfersoncoelho@gmail.com>
2019-05-02 16:49:54 -0300

    atualizando o jwtweb token e criando a rota para authenticar user e para login diretamente, update de codigo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,13 @@ const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
 
+const authConfig = require('../config/auth')
+
 const router = express.Router();
+
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret,{ expiresIn: 86400, });
+}
 
 router.post('/register', async (req, res) => {
 
@@ -21,7 +27,11 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-        return res.send({user});
+        return res.send({
+            user, 
+            token: generateToken({id: user.id}),
+        });
+    
     } catch (err) {
 
         return res.status(400).send({error: 'Registration failed'});
@@ -45,7 +55,10 @@ router.post('/authenticate', async(req, res) =>{
 
     user.password = undefined;
 
-    res.send({user});
+    res.send({
+        user, 
+        token: generateToken({id: user.id}),
+    });
 
 
 
